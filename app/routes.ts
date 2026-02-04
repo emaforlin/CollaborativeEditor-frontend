6ca0262e01c20cfd8589aff09cfbbff64c236514
Emanuel Forlin <emaa.forlin@gmail.com>
2026-02-03 23:16:07 -0300

    feat: add not found page

--- app/routes.ts
+++ app/routes.ts
@@ -3,6 +3,7 @@ import { type RouteConfig, index, route } from "@react-router/dev/routes";
 export default [
     index("routes/dashboard.tsx"),
     route("documents", "routes/documents.tsx"),
-    route("editor", "routes/editor.tsx"),
+    route("editor/:documentID", "routes/editor.tsx"),
     route("login", "routes/login.tsx"),
+    route("*", "routes/not_found.tsx"),
 ] satisfies RouteConfig;
